f5e309ffc390247fcaa6d41561c8bd1b6dfc1477
b2un0 <branko.wilhelm@gmail.com>
2013-11-21 22:53:48 +0100

    enh: click events

--- tmpl/default.js
+++ tmpl/default.js
@@ -1,11 +1,11 @@
 if (jQuery) {
     jQuery(document).ready(function ($) {
-        $('.mod_wow_raid_progress_mop .header').click(function () {
-            if (jQuery(this).next('li').is(':visible')) {
-                jQuery(this).next('li').slideUp('slow');
+        $('.mod_wow_raid_progress_mop .header span').click(function () {
+            if (jQuery(this).parent().next('li').is(':visible')) {
+                jQuery(this).parent().next('li').slideUp('slow');
             } else {
                 jQuery('.mod_wow_raid_progress_mop .npcs').slideUp('slow');
-                jQuery(this).next('li').slideToggle('slow');
+                jQuery(this).parent().next('li').slideToggle('slow');
             }
         });
     });
